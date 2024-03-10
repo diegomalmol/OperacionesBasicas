b37a79f4b004ff35f365d1e3a6c12d242be38c5b
diegomalmol <159237163+diegomalmol@users.noreply.github.com>
2024-03-10 18:26:48 +0100

    Update OperacionesBasicas.java
    Añadimos la función de sumar.

--- OperacionesBasicas.java
+++ OperacionesBasicas.java
@@ -15,6 +15,15 @@ public class OperacionesBasicas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a=5, b=4;
+        int resultado; resultado = sumar(a,b);
+        System.out.println(a+"+"+b"="+resultado);
+            
+    }
+    public static int sumar(int a, int b){
+        int resultado;
+        resultado = a+b;
+        return resultado;
     }
     
 }
